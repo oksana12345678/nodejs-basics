320a35a80f57655950bc49f614bef21e4e154044
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-07-07 22:49:00 +0200

    add

---
+++ src/controllers/students.js
@@ -0,0 +1,25 @@
+import { getAllStudents, getStudentById } from '../services/students.js';
+
+export const getStudentsController = async (req, res) => {
+  const students = await getAllStudents();
+  res.json({
+    status: 200,
+    message: 'Successfully found students!',
+    data: students,
+  });
+};
+
+export const getStudentByIdController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const student = await getStudentById(studentId);
+
+  if (!student) {
+    next(new Error('Student not found'));
+    return;
+  }
+  res.json({
+    status: 200,
+    message: `Successfully found student with id ${studentId}!`,
+    data: student,
+  });
+};
--- src/routers/students.js
+++ src/routers/students.js
@@ -1,28 +1,13 @@
 import { Router } from 'express';
-import { getAllStudents, getStudentById } from '../services/students.js';
+import {
+  getStudentByIdController,
+  getStudentsController,
+} from '../controllers/students.js';
 
 const studentRouter = Router();
 
-studentRouter.get('/students', async (req, res) => {
-  const students = await getAllStudents();
-  res.status(200).json({
-    data: students,
-  });
-});
+studentRouter.get('/students', getStudentsController);
 
-studentRouter.get('/students/:studentId', async (req, res) => {
-  const { studentId } = req.params;
-  const student = await getStudentById(studentId);
-
-  if (!student) {
-    res.status(404).json({
-      message: 'Student not found',
-    });
-    return;
-  }
-  res.status(200).json({
-    data: student,
-  });
-});
+studentRouter.get('/students/:studentId', getStudentByIdController);
 
 export default studentRouter;
