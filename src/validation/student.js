ce7533caa765348d775b0288600fe8f8ca221e73
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-07-11 22:11:28 +0200

    add

--- src/validation/student.js
+++ src/validation/student.js
@@ -1,59 +1,59 @@
 import Joi from 'joi';
-// import { model, Schema } from 'mongoose';
+import { model, Schema } from 'mongoose';
 
-// const studentSchema = new Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: true,
-//     },
-//     age: {
-//       type: Number,
-//       required: true,
-//     },
-//     gender: {
-//       type: String,
-//       required: true,
-//       enum: ['male', 'female', 'other'],
-//     },
-//     avgMark: {
-//       type: Number,
-//       required: true,
-//     },
-//     onDuty: {
-//       type: Boolean,
-//       required: true,
-//       default: false,
-//     },
-//   },
-//   {
-//     timestamps: true,
-//     versionKey: false,
-//   },
-// );
+const studentSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ['male', 'female', 'other'],
+    },
+    avgMark: {
+      type: Number,
+      required: true,
+    },
+    onDuty: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
 
-// const StudentCollection = model('students', studentSchema);
-// export default StudentCollection;
+const StudentCollection = model('students', studentSchema);
+export default StudentCollection;
 
 export const createStudentSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required().message({
-    'string.base': 'Username should be a string!',
-    'string.min': 'Username should have at least {#limit} characters!',
-    'string.max': 'Username should have at most {#limit} characters!',
-    'any.required': 'Username is required!',
+  name: Joi.string().min(3).max(30).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
   }),
   email: Joi.string().email().required(),
-  age: Joi.number().integer().min(6).max(16).required().message({
+  age: Joi.number().integer().min(6).max(16).required().messages({
     'number.base': 'Age should be a number!',
     'number.integer': 'Number should be integer!',
     'number.min': 'Age should have at least {#limit}',
     'number.max': 'Age should have at most {#limit}',
     'any.required': 'Age is required!',
   }),
-  gender: Joi.string().valid('male', 'female', 'other').required().message({
+  gender: Joi.string().valid('male', 'female', 'other').required().messages({
     'string.base': 'Gender should be a string',
   }),
-  avgMark: Joi.number().min(2).max(12).required().message({
+  avgMark: Joi.number().min(2).max(12).required().messages({
     'number.base': 'Mark should be a number!',
     'number.min': 'Mark should have at list {#limit}',
     'number.max': 'Mark should have at most {#limit}',
@@ -63,22 +63,22 @@ export const createStudentSchema = Joi.object({
 });
 
 export const updateStudentSchema = Joi.object({
-  name: Joi.string().min(3).max(30).message({
+  name: Joi.string().min(3).max(30).messages({
     'string.base': 'Username should be a string!',
     'string.min': 'Username should have at least {#limit} characters!',
     'string.max': 'Username should have at most {#limit} characters!',
   }),
   email: Joi.string().email(),
-  age: Joi.number().integer().min(6).max(16).message({
+  age: Joi.number().integer().min(6).max(16).messages({
     'number.base': 'Age should be a number!',
     'number.integer': 'Number should be integer!',
     'number.min': 'Age should have at least {#limit}',
     'number.max': 'Age should have at most {#limit}',
   }),
-  gender: Joi.string().valid('male', 'female', 'other').message({
+  gender: Joi.string().valid('male', 'female', 'other').messages({
     'string.base': 'Gender should be a string',
   }),
-  avgMark: Joi.number().min(2).max(12).message({
+  avgMark: Joi.number().min(2).max(12).messages({
     'number.base': 'Mark should be a number!',
     'number.min': 'Mark should have at list {#limit}',
     'number.max': 'Mark should have at most {#limit}',
